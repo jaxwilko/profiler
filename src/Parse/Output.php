9dae8e2c899c77137721960e23d26d8fa2b2836d
Jack Wilkinson <me@jackwilky.com>
2022-04-11 15:57:43 +0100

    Added pre-json encode utf8 conversion of profile values

--- src/Parse/Output.php
+++ src/Parse/Output.php
@@ -13,6 +13,32 @@ class Output
     public function create(array $profile)
     {
         $content = file_get_contents(__DIR__ . '/html.stub');
-        return str_replace(['%%data%%'], [json_encode($profile)], $content);
+        return str_replace('%%data%%', json_encode($this->utf8Encode($profile)), $content);
+    }
+
+    public function utf8Encode(mixed $data): mixed
+    {
+        if (is_string($data)) {
+            return mb_convert_encoding($data, 'UTF-8', 'UTF-8');
+        }
+
+        if (is_array($data)) {
+            $return = [];
+            foreach ($data as $index => $value) {
+                $return[$index] = $this->utf8Encode($value);
+            }
+
+            return $return;
+        }
+
+        if (is_object($data)) {
+            foreach ((array) $data as $index => $value) {
+                $data->{$index} = $this->utf8Encode($value);
+            }
+
+            return $data;
+        }
+
+        return $data;
     }
 }
